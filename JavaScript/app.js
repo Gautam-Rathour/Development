b783dcdd99bdbb0aca518d2392b5ea99f1a81685
Gautam-Rathour <gau321tam@gmail.com>
2025-03-18 20:30:16 +0530

    add

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,12 +1,58 @@
-console.log("Hello World");
-console.log("Apna Collage");
+// console.log("Hello World");
+// console.log("Raju bhai");
 
-let a = 5;
-let b = 6;
-let sum = a + b;
-console.log(sum);
+// let a = 10;
+// let b = 5;
+
+// console.log(a++); //10
+// console.log(++a); //12
+
+// ===========================================================================
+// ===========================================================================
+
+// let age = 19; 
+// console.log(age >= 18);
+// console.log(5 === "5");
+// console.log('a'+1);
+
+// ===========================================================================
+// ===========================================================================
+
+// Conditional Statements
+// console.log("Before my if statement");
+// let age = 34;
+// if(age >= 18) {
+//     console.log("you can vote");
+//     console.log("you can drive");
+// } else {
+//     console.log("you can't vote");
+//     console.log("you can't drive");
+// }
+// console.log("After my if statement");
+
+// ===========================================================================
+// ===========================================================================
+
+// let str = "ample";
+
+// let leng = str.length;
+// // console.log(leng); //6
+
+// if(str[0] == 'a' && leng >= 3) {
+//     console.log("This is a good string");
+// } else {
+//     console.log("This is not a good string");
+// }
+
+// ===========================================================================
+// ===========================================================================
+
+if(true) {
+    console.log("This is true");
+} else {
+    console.log("This is false");
+}
 
 
 
 
-alert();
