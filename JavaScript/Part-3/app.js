0ef24114fca85ce19e822ee0199ef746ce51b39d
Gautam-Rathour <gau321tam@gmail.com>
2025-03-20 08:24:21 +0530

    Add

--- JavaScript/Part-3/app.js
+++ JavaScript/Part-3/app.js
@@ -1,4 +1,23 @@
-let msg = "   Hello     ";
-console.log(msg);
-let trimed = msg.trim();
-console.log(trimed);
+// let msg = "   Hello     ";
+// console.log(msg);
+// let trimed = msg.trim();
+// console.log(trimed);
+
+// ===========================================================================
+// ===========================================================================
+
+// let Name = "Gautam Babu";
+// let msg = "error";
+
+// console.log(msg.toUpperCase());
+// console.log(Name.toLowerCase());
+
+let msg = "ILoveCoding";
+
+let ans1 = msg.indexOf("Love");
+let ans2 = msg.indexOf("o");
+let ans3 = msg.indexOf("z");
+
+console.log(ans1);
+console.log(ans2);
+console.log(ans3);
