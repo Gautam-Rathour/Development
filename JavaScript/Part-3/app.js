1760306c1f7c97c4ed5e5249edb4cecb989117a6
Gautam-Rathour <gau321tam@gmail.com>
2025-03-23 11:15:00 +0530

    Add Some Code

--- JavaScript/Part-3/app.js
+++ JavaScript/Part-3/app.js
@@ -5,23 +5,138 @@
 
 // ===========================================================================
 // ===========================================================================
+// // toUpperCase, toLowerCase  : ---
 
 // let Name = "Gautam Babu";
 // let msg = "error";
-
 // console.log(msg.toUpperCase());
 // console.log(Name.toLowerCase());
 
-let msg = "ILoveCoding";
+// ===========================================================================
+// Find index of : ---
 
-let ans1 = msg.indexOf("Love");
-let ans2 = msg.indexOf("o");
-let ans3 = msg.indexOf("z");
-
-console.log(ans1);
-console.log(ans2);
-console.log(ans3);
-
+// let msg = "ILoveCoding";
+// let ans1 = msg.indexOf("Love");
+// let ans2 = msg.indexOf("o");
+// let ans3 = msg.indexOf("z");
+// console.log(ans1);
+// console.log(ans2);
+// console.log(ans3);
 
 // ===========================================================================
 // ===========================================================================
+// // Method Changing  : ---
+
+// let msg = "              hello    ";
+// // console.log("befor change : " , msg);
+// // let newMsg = msg.trim();
+// // console.log("after trim : ", newMsg);
+// // newMsg = newMsg.toUpperCase();
+// // console.log("after Uppercase : ", newMsg);
+// newMsg = msg.trim().toUpperCase();
+// console.log(newMsg)
+
+// ===========================================================================
+// // Slice  : -
+
+// let str = "ILoveCoding";
+// // let ans = str.slice(5, str.length);
+// let ans = str.slice(-11);
+// console.log(ans);
+
+// ===========================================================================
+// // Replace  :  --
+
+// let msg = "ILoveCoding";
+// let ans = msg.replace("Love", "do");
+// console.log(ans);
+
+// ===========================================================================
+// // Repeat  :  --
+
+// let str = "Mango";
+// let ans = str.repeat(3);
+// console.log(ans); // output :  "MangoMangoMango"  // 3
+
+// ===========================================================================
+// // Practice Qs  :  --
+
+// let msg = "help!";
+// let ans = msg.trim().toUpperCase();
+// console.log(ans); // output :  "HELP!"  // 5
+
+// let Name = "ApnaCollege";
+// let ans = Name.slice(4, 9);
+// console.log(ans);
+// let ans = Name.indexOf("na");
+// console.log(ans);
+// let ans = Name.replace("Apna", "Our");
+// console.log(ans);
+// let ans = Name.slice(4);
+// console.log(ans);
+// let ans = Name.replace("l", "t").replace("l", "t");
+// console.log(ans);
+
+// ===========================================================================
+// ===========================================================================
+// Array  :  --
+
+// let Students = ["aman", "shradha", "rajat"];
+// console.log(Students);
+
+// let nums = [2, 4, 6, 8];
+// console.log(nums);
+// console.log(nums[3]);
+// console.log(nums.length);
+// console.log(typeof(nums));
+
+// let info = ["shradha", 23, 89.9];
+// console.log(info);
+// console.log(info[0].length);
+
+// let empArr = [];
+// console.log(empArr);
+
+// // ===========================================================================
+// // Array are Mutable   :  --
+
+// let fruits = ["mango", "apple", "orange"];
+// fruits[0] = "barana";
+// console.log(fruits);
+// fruits[10] = "gowava";
+// console.log(fruits);
+// console.log(fruits.length);
+
+
+// let cars = ["audi", "bmw", "xuv", "maruti"];
+// cars.push("toyota");
+// console.log(cars);
+// cars.unshift("honda");
+// console.log(cars);
+// cars.pop();
+// console.log(cars);
+// cars.shift();
+// console.log(cars);
+
+// Practice Qs : --
+let month = ["january", "july", "march", "august"];
+month.shift();
+month.shift();
+month.unshift("june");
+month.unshift("july");
+console.log(month);
+
+// // ===========================================================================
+// // ===========================================================================
+
+
+
+
+
+
+
+
+
+
+
+
