34dc2e8d0df5fad7044d15802c9d928ae4d78787
Gautam-Rathour <gau321tam@gmail.com>
2025-04-28 12:20:17 +0530

    Add Practic File

---
+++ EJSDIR/Ex/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.get("/", function(req, res)  {
+    res.render('index',{ name : "Rupendar"});
+})
+
+
+app.listen(3000);
