184a13d3656c67215f29f8e8465fd0fce87f075e
Gautam-Rathour <gau321tam@gmail.com>
2025-05-06 21:14:10 +0530

    Add

--- SQL/SQLCLASS/index.js
+++ SQL/SQLCLASS/index.js
@@ -1,20 +1,39 @@
 
 // CJS
 const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2");
 
-let createRandomUser = () => {
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'cohort_app',
+    password: "qowQez-0novti-zivgyr"
+  });
+
+  try {
+    connection.query("SHOW TABLES", (err, result) => {
+        if(err) throw err;
+        console.log("This is the table : - " , result);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+  
+  connection.end();
+
+
+  
+let getRandomUser = () => {
   return {
     userId: faker.string.uuid(),
     username: faker.internet.username(), // before version 9.1.0, use userName()
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
+    password: faker.internet.password()
   };
 }
 
-console.log(createRandomUser());
+// console.log(getRandomUser());
 
 
 
