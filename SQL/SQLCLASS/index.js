76c328371a2031f25b3b918f5e6a1d5f49015869
Gautam-Rathour <gau321tam@gmail.com>
2025-05-07 06:36:47 +0530

    Add SQL file

--- SQL/SQLCLASS/index.js
+++ SQL/SQLCLASS/index.js
@@ -11,10 +11,35 @@ const connection = mysql.createConnection({
     password: "qowQez-0novti-zivgyr"
   });
 
+  let getRandomUser = () => {
+    return [
+      faker.string.uuid(),
+      faker.internet.username(), // before version 9.1.0, use userName()
+      faker.internet.email(),
+      faker.internet.password(),
+    ];
+  };
+
+  // Inserting New Data
+  let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+//   let users = [
+//                 ["123b", "123_newuserb", "abc@gmail.comb", "abcb"],
+//                 ["123c", "123_newuserc", "abc@gmail.comc", "abcc"]
+//             ];
+  
+let data = [];
+for(let i=1; i<=100; i++) {
+    data.push(getRandomUser());  // 100 faker users
+}
+
+
   try {
-    connection.query("SHOW TABLES", (err, result) => {
+    connection.query(q, [data], (err, result) => {
         if(err) throw err;
-        console.log("This is the table : - " , result);
+        console.log(result);
+        console.log(result.length);
+        console.log(result[0]);
+        console.log(result[1]);
       });
   } catch (err) {
     console.log(err);
@@ -23,15 +48,8 @@ const connection = mysql.createConnection({
   connection.end();
 
 
-  
-let getRandomUser = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    password: faker.internet.password()
-  };
-}
+
+
 
 // console.log(getRandomUser());
 
