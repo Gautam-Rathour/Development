f921cca81e3b944f01264cca0a701ff9e1dd781e
Gautam-Rathour <gau321tam@gmail.com>
2025-05-12 14:39:28 +0530

    Add few file

---
+++ MONGO/MongoDB_with_Express/init.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat");
+
+main()
+    .then(() => {
+        console.log("Connection successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+let allChats = [
+    {
+    from: "neha",
+    to: "priya",
+    msg: "send me your exam sheets",
+    created_at: new Date(),
+    },
+    {
+    from: "amit",
+    to: "ramesh",
+    msg: "bring me some fruits",
+    created_at: new Date(),
+    },
+    {
+    from: "anita",
+    to: "ramesh",
+    msg: "send me your exam sheets",
+    created_at: new Date(),
+    },
+    {
+    from: "tony",
+    to: "priya",
+    msg: "send me your exam sheets",
+    created_at: new Date(),
+    },
+];
+
+Chat.insertMany(allChats);
+
+
+
+
+
+
+
