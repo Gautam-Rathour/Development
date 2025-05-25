7da91f7e3d85dfbd58909800e52a4c4e807eb198
Gautam-Rathour <gau321tam@gmail.com>
2025-05-25 19:38:07 +0530

    Add

--- Middlewares/app.js
+++ Middlewares/app.js
@@ -2,17 +2,42 @@ const express = require("express");
 const app = express();
 
 // Middleware => response send
+// app.use((req, res, next) => {
+//     console.log("Hi, I am 1st middleware");
+//     next();
+// });
 
+// app.use((req, res, next) => {
+//     console.log("Hi, I am 2nd middleware");
+//     next();
+// });
+
+
+// // logger - morgan
+// app.use((req, res, next) => {
+//     req.time = new Date(Date.now());
+//     console.log(req.method, req.hostname, req.path, req.time);
+//     next();
+// })
+
+
+app.use("/random", (req, res, next) => {
+    console.log("I am only for random page");
+    next();
+});
 
 app.get("/", (req, res) => {
-    res.send("hello mr.");
+    res.send("Hi, I am root.");
+})
+
+app.get("/random", (req, res) => {
+     res.send("This is a random page");
 })
 
 
-
-
-
-
+app.use((req, res) => {
+    res.send("Page not found");
+})
 
 const port = 8080;
 app.listen(port, () => {
