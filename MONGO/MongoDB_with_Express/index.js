acffd4c6804130ed9065b4e1ddd126ac389c168e
Gautam-Rathour <gau321tam@gmail.com>
2025-05-31 09:25:14 +0530

    Add Folder Relationships

--- MONGO/MongoDB_with_Express/index.js
+++ MONGO/MongoDB_with_Express/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError");
 
 
 app.set("views", path.join(__dirname, "views"));
@@ -21,28 +22,26 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+    await mongoose.connect("mongodb://127.0.0.1:27017/fakewhatsapp");
 }
 
 
 // Index Route
-app.get('/chats', async (req, res) => {
-  try {
-    const chats = await Chat.find({});
-    console.log(chats); // okay to log to console
-    res.render("index.ejs", { chats });    // only one response sent
-  } catch (err) {
-    res.status(500).send('Something went wrong');
-  }
-});
+app.get('/chats', asyncWrap( async (req, res) => {
+    let chats = await Chat.find({});
+    res.render("index.ejs", { chats });    
+
+}));
 
 // New Route
 app.get("/chats/new", (req, res) => {
+  // throw new ExpressError(404, "Page not found");
   res.render("new.ejs");
 })
 
 // Create Route
-app.post("/chats", (req, res) => {
+app.post("/chats", asyncWrap( async (req, res, next) => {
+  
    let { from, to, msg } = req.body;
    let newChat = new Chat({
     from: from,
@@ -51,29 +50,59 @@ app.post("/chats", (req, res) => {
     created_at: new Date()
    });
 
-   newChat
-    .save()
-    .then(res => {
-      console.log("chat was saved");
-    }).catch(err => {
-      console.log(err);
-    });
+   await newChat.save();
     res.redirect("/chats");
+
+}));
+
+
+const handleValidationErr = (err) => {
+  console.log("This was a CastError. Please check your input.");
+  return err;
+}
+
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if (err.name === "CastError") {
+    err = handleValidationErr(err)
+  }
+  next(err);
 });
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Some Error Occurred" } = err;
+  res.status(status).send(message);
+});
+
+
+function asyncWrap(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  }
+}
+
+// NEW - Show Route
+app.get("/chats/:id", asyncWrap( async (req, res, next) => {
+    let { id } = req.params;
+  let chat = await Chat.findById(id);
+  // if(!chat) {
+  //   return next(new ExpressError(404, "chat not found"));
+  // }
+  res.render("edit.ejs", { chat }); // send 'chat' to the view
+}));
 
 //Edit Route
-app.get("/chats/:id/edit", async (req, res) => {
-    const { id } = req.params;
-    try {
-        const chat = await Chat.findById(id);
-        res.render("edit", { chat }); // send 'chat' to the view
-    } catch (err) {
-        res.send("Chat not found");
-    }
-});
+app.get("/chats/:id/edit", asyncWrap( async (req, res) => {
+      const { id } = req.params;
+      const chat = await Chat.findById(id);
+      res.render("edit", { chat }); // send 'chat' to the view
+  
+}));
 
 //Update Route 
-app.put("/chats/:id", async (req, res) => {
+app.put("/chats/:id", asyncWrap( async (req, res) => {
+
   let { id } = req.params;
   let { msg: newMsg } = req.body;
   console.log(newMsg);
@@ -86,37 +115,20 @@ app.put("/chats/:id", async (req, res) => {
 
   console.log(updatedChat);
   res.redirect("/chats");
-})
+}));
 
 // Destroy Route
-app.delete("/chats/:id", async (req, res) => {
-  let { id } = req.params;
+app.delete("/chats/:id", asyncWrap( async (req, res) => {
+    let { id } = req.params;
   let deletedChat = await Chat.findByIdAndDelete(id);
   console.log(deletedChat);
   res.redirect("/chats");
-});
+  
+}));
 
 app.get("/", (req, res) => {
     res.send("Root is Working");
 });
-
-
-
-// let chat1 = new Chat({
-//     from: "neha",
-//     to: "priya",
-//     msg: "send me your exam sheets",
-//     created_at: new Date(),
-// });
-
-
-// chat1.save()
-//     .then((res) => {
-//         console.log(res);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
 
 
 
---
+++ Relationships/Models/user.js
@@ -0,0 +1,52 @@
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const URL = 'mongodb://127.0.0.1:27017/relationDemo';
+
+mongoose.connect(URL)
+    .then( function() {
+        console.log("Connected to MongoDB!");
+    })
+    .catch( function(err) {
+        console.long("Connection failed: " + err);
+    });
+
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+            locations: String,
+            city: String,
+        },
+    ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUsers = async() => {
+    let user1 = new User ({
+        username: "sherlockholmes",
+        addresses: [
+            {
+            locations: "221b Baker Street",
+            city: "London"
+            },
+        ],
+    });
+
+    user1.addresses.push({locations: "P32 WallStreet", city: "London"});
+    let result = await user1.save();
+    console.log(result);
+}
+
+addUsers();
+
+
+
+
+
+
+
