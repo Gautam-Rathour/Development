b517e41515e420ac0667d49f6397b10278fef4e7
Gautam-Rathour <gau321tam@gmail.com>
2025-06-02 19:19:08 +0530

    Add Project Code

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -7,7 +7,9 @@ const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
+
 
 
 
@@ -44,6 +46,17 @@ const validateListing = (req, res, next) => {
         next();
     }
 }
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
 //Index Route
 app.get("/listings", wrapAsync( async (req, res) => {
     const allListings = await Listing.find({});
@@ -58,7 +71,7 @@ app.get("/listings/new", (req, res) => {
 // Show Route 
 app.get("/listings/:id", wrapAsync( async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show", { listing })
 }));
 
@@ -93,6 +106,20 @@ app.delete("/listings/:id", wrapAsync( async (req, res) => {
     res.redirect("/listings");
 }));
 
+
+// Reviews
+// Post Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync( async(req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
