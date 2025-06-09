a3de51d2f7452a040f87033519ef0941120a3b1d
Gautam-Rathour <gau321tam@gmail.com>
2025-06-09 20:14:20 +0530

    Add ph ase-3 (Part-B) Code

--- MajorProject/controllers/listings.js
+++ MajorProject/controllers/listings.js
@@ -1,4 +1,9 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding'); // ✅ correct
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
+
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
@@ -30,15 +35,29 @@ module.exports.showListing = async (req, res) => {
 
 
 module.exports.createListing = async (req, res, next) => {
+    // geocoding in structured input mode
+    let response = await geocodingClient.forwardGeocode({
+    query: req.body.listing.location,
+    limit: 1,
+    })
+    .send();
+    
+
+    res.send("done!");
+
+
     let url = req.file.path;
     let filename = req.file.filename;
-        
-        const newListing = new Listing(req.body.listing);
-        newListing.owner = req.user._id;
-        newListing.image = { url, filename };
-        await newListing.save();
-        req.flash("success", "New Listing Created!");
-        res.redirect("/listings");
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
+
+    newListing.geometry = response.body.features[0].geometry;
+
+    let savedListing = await newListing.save();
+    console.log(savedListing);
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
 }
 
 
@@ -49,7 +68,10 @@ module.exports.renderEditForm = async (req, res) => {
         req.flash("error", "Listing you requested for does not exist!");
         res.redirect("/listings");
     }
-    res.render("listings/edit", { listing });
+
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_350");
+    res.render("listings/edit", { listing, originalImageUrl });
 }
 
 
