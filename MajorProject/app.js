b19b2a113becf9bd58e16c0f2c2fec40a4c3b8d4
Gautam-Rathour <gau321tam@gmail.com>
2025-06-10 21:25:52 +0530

    Add

--- MajorProject/app.js
+++ MajorProject/app.js
@@ -62,9 +62,9 @@ const sessionOptions = {
 
 
 
-app.get("/", (req, res) => {
-    res.send("Hi, I am Root!");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hi, I am Root!");
+// });
 
 
 app.use(session(sessionOptions));
